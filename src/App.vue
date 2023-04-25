1e726c13459600e16bdb8d74b0ddd19e54ceaabc
Sungsoon Park <sungsoon.park@gmail.com>
2023-04-24 20:29:02 -0700

    using toRefs

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
-import products from "@/data/products.json";
+// import products from "@/data/products.json";
 
 import { useProductStore } from '@/stores/ProductStore.js'
+// import { toRefs } from 'vue'
 
 const productStore = new useProductStore()
+// const { products } = toRefs(productStore)
 
 </script>
 
@@ -13,6 +15,7 @@ const productStore = new useProductStore()
   <div class="container">
     <TheHeader />
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
+      <!-- <ProductCard v-for="product in products" :key="product.name" :product="product" /> -->
       <ProductCard v-for="product in productStore.products" :key="product.name" :product="product" />
     </ul>
   </div>
